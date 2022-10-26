4677e2105ef22258ac5880ea53921cb4ca11d38e
rodro2 <rodrigovela22@gmail.com>
2022-10-26 20:23:57 -0300

    mercado

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.use(express.static("public"))
+
+app.listen(3030, ()=> console.log("server corriendo"));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./views/index.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve("./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("./views/login.html"));
+});
+
+
+//app.use("/static", express.static(__dirname + "/public"));//
+
+/*boton formulario para volver a inicio
+app.post("/login", (req,res)=>{
+    res.redirect("/")
+}) */
